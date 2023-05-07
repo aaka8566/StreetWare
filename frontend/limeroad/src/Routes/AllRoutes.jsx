0b4f9d95459fff02983ae2afb284e31cd58f715c
Vidhi Sharma <vidhisharma@192.168.0.107>
2023-05-07 13:54:13 +0530

    day1

--- frontend/limeroad/src/Routes/AllRoutes.jsx
+++ frontend/limeroad/src/Routes/AllRoutes.jsx
@@ -7,20 +7,22 @@ import {Products} from "../Pages/Products";
 import {Signup} from "../Pages/Signup";
 import {AdminHome} from "../Pages/Admin/AdminHome";
 import {AdminProducts} from "../Pages/Admin/AdminProducts";
+import AdminLogin from '../Pages/Admin/AdminLogin';
 
 export const AllRoutes = () => {
   return (
     <div>
-        <Routes>
-            <Route path='/' element={<Home/>}></Route>
-            <Route path='/products' element={<Products/>}></Route>
-            <Route path='/signup' element={<Signup/>}></Route>
-            <Route path='/cart' element={<Cart/>}></Route>
-            <Route path='/payment' element={<Payment/>}></Route>
-            <Route path='/adminhome' element={<AdminHome/>}></Route>
-            <Route path='/adminproducts' element={<AdminProducts/>}></Route>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/products" element={<Products />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/payment" element={<Payment />}></Route>
+        <Route path="/adminhome" element={<AdminHome />}></Route>
+        <Route path="/adminproducts" element={<AdminProducts />}></Route>
+        <Route path="/adminlogin" element={<AdminLogin/>}></Route>
+      </Routes>
     </div>
-  )
+  );
 }
 
